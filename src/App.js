8a5b6d3e2e801607f9e0a6faef9ac3ea46e06e00
Viktoriya Chernookaya <32044161+viktoria2506@users.noreply.github.com>
2020-10-01 15:50:16 +0300

    show users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import UsersData from './data/users.json';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function User (props) {
+    return (
+        <div className={'UserInfo'}>
+            <h2>{props.name}</h2>
+            <h4>{props.username}</h4>
+            <h4>phone: {props.phone}</h4>
+            <p>{props.email}</p>
+            <i>{props.website}</i>
+            <Address address={props.address}/>
+            <Company company={props.company}/>
+        </div>
+    );
+}
+
+function Address (props) {
+    return (
+        <div>
+            <p>{props.address.street}, {props.address.suite}, {props.address.city}, {props.address.zipcode}</p>
+            <Geo geo={props.address.geo}/>
+        </div>
+    );
+}
+
+function Geo (props) {
+    return (
+        <div>
+            <p>geo: {props.geo.lat}, {props.geo.lng}</p>
+        </div>
+    );
+}
+
+function Company (props) {
+    return (
+        <div>
+            <h4>{props.company.name}</h4>
+            <p>catchPhrase: {props.company.catchPhrase}</p>
+            <p>bs: {props.company.bs}</p>
+        </div>
+    )
+}
+
+function App () {
+    return (
+        <div className="App">
+            {UsersData.map((user, index) => {
+                return <div>
+                    <User name={user.name}
+                          username={user.username}
+                          email={user.email}
+                          address={user.address}
+                          phone={user.phone}
+                          website={user.website}
+                          company={user.company}
+                    />
+                    <hr/>
+                </div>;
+            })}
+
+        </div>
+    );
 }
 
 export default App;
